767e1152d27bbfddd7794a36861b1c8593a3b77c
chelseydenton <dentonchelsey@gmail.com>
2014-04-16 13:01:15 -0500

    show microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ActiveRecord::Base
 	validates :user_id, presence: true
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
+	validates :content, presence: true, length: { maximum: 140 }
 end
